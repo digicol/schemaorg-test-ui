1e3acf25379cf2a3657298366a73158bf9bfcce2
Tim Strehle <tim@strehle.de>
2016-05-20 13:19:02 +0200

    First working version of the test UI. Configure content sources in app/config/parameters.yml.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,143 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        $utils = $this->get('app.utils');
+
+        $content_sources = $this->getParameter('content_sources');
+
+        foreach ($content_sources as $key => $content_source_params)
+        {
+            $content_source = $utils->newContentSource($content_source_params);
+
+            $content_sources[ $key ][ 'search_actions' ] = $content_source->describeSearchActions();
+        }
+
+        $tpl_vars =
+            [
+                'content_sources' => $content_sources
+            ];
+
+        return $this->render('default/index.html.twig', $tpl_vars);
+    }
+
+
+    /**
+     * @Route("/details/{content_source_key}/{thing_uri}", name="details")
+     */
+    public function detailsAction(Request $request, $content_source_key, $thing_uri)
+    {
+        $thing_uri = urldecode($thing_uri);
+        
+        $tpl_vars =
+            [
+                'content_source_key' => $content_source_key
+            ];
+
+        $utils = $this->get('app.utils');
+
+        $content_sources = $this->getParameter('content_sources');
+
+        if (! isset($content_sources[ $content_source_key ]))
+        {
+            throw $this->createNotFoundException('Content source does not exist.');
+        }
+
+        $tpl_vars[ 'content_source_params' ] = $content_sources[ $content_source_key ];
+
+        $content_source = $utils->newContentSource($content_sources[ $content_source_key ]);
+
+        $thing = $content_source->newThing($thing_uri);
+
+        $tpl_vars[ 'thing' ] = $utils->getThingTemplateVars($thing);
+
+        return $this->render('default/details.html.twig', $tpl_vars);
+    }
+
+
+    /**
+     * @Route("/search/{content_source_key}/{search_action_key}", name="search")
+     */
+    public function searchAction(Request $request, $content_source_key, $search_action_key)
+    {
+        $query = $request->query->get('q', '');
+        $page = $request->query->get('p', 1);
+
+        $tpl_vars =
+            [
+                'content_source_key' => $content_source_key,
+                'search_action_key' => $search_action_key,
+                'q' => $query
+            ];
+
+        $utils = $this->get('app.utils');
+
+        $content_sources = $this->getParameter('content_sources');
+        
+        if (! isset($content_sources[ $content_source_key ]))
+        {
+            throw $this->createNotFoundException('Content source does not exist.');
+        }
+
+        $tpl_vars[ 'content_source_params' ] = $content_sources[ $content_source_key ];
+
+        $content_source = $utils->newContentSource($content_sources[ $content_source_key ]);
+
+        $search_actions = $content_source->describeSearchActions();
+
+        if (! isset($search_actions[ $search_action_key ]))
+        {
+            throw $this->createNotFoundException('Search action does not exist.');
+        }
+
+        $tpl_vars[ 'search_action_params' ] = $search_actions[ $search_action_key ];
+
+        $search_action = $content_source->newSearchAction($search_actions[ $search_action_key ]);
+
+        $search_action->setInputProperties
+        (
+            [
+                'q' => $query,
+                'opensearch:count' => 10,
+                'opensearch:startPage' => $page
+            ]
+        );
+
+        $search_action->execute();
+
+        $search_result = [ ];
+
+        foreach ($search_action->getResult() as $thing)
+        {
+            $search_result[ ] = $utils->getThingTemplateVars($thing);
+        }
+
+        $tpl_vars[ 'search_result' ] = $search_result;
+
+        $tpl_vars[ 'search_result_meta' ] = $search_action->getResultMeta();
+
+        $this->addPaginationTemplateVars($tpl_vars);
+
+        return $this->render('default/search.html.twig', $tpl_vars);
+    }
+
+
+    protected function addPaginationTemplateVars(&$tpl_vars)
+    {
+        $items_per_page = $tpl_vars[ 'search_result_meta' ][ 'opensearch:itemsPerPage' ];
+
+        $current_page = ceil($tpl_vars[ 'search_result_meta' ][ 'opensearch:startIndex' ] / $items_per_page);
+        $last_page = max(1, ceil($tpl_vars[ 'search_result_meta' ][ 'opensearch:totalResults' ] / $items_per_page));
+        $previous_page = max(1, ($current_page - 1));
+        $next_page = min($last_page, ($current_page + 1));
+
+        $pages = range(1, max($current_page, min(10, $last_page)));
+
+        $tpl_vars[ 'pagination' ] =
+            [
+                'pages' => $pages,
+                'current_page' => $current_page, 
+                'next_page' => $next_page,
+                'previous_page' => $previous_page
+            ];
     }
 }
---
+++ src/AppBundle/Utils.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace AppBundle;
+
+
+class Utils
+{
+    /**
+     * @param array $params
+     * @return \Digicol\SchemaOrg\AdapterInterface
+     */
+    public function newContentSource(array $params)
+    {
+        $classname = $params[ 'classname' ];
+
+        if ((! empty($params[ 'autoload_prefix' ])) && (! empty($params[ 'autoload_basedir' ])))
+        {
+            $this->registerAutoloadPrefix($params[ 'autoload_prefix' ], $params[ 'autoload_basedir' ]);
+        }
+
+        if (! empty($params[ 'autoload_file' ]))
+        {
+            $this->registerAutoloadFile($params[ 'autoload_file' ]);
+        }
+        
+        return new $classname($params);
+    }
+    
+    
+    public function getThingTemplateVars(\Digicol\SchemaOrg\ThingInterface $thing, array $params = [ ])
+    {
+        $type = $thing->getType();
+        $properties = $thing->getProperties();
+
+        return
+            [
+                'type' => $type,
+                'properties' => $properties,
+                'reconciled' => \Digicol\SchemaOrg\Utils::reconcileThingProperties($type, $properties)
+            ];
+    }
+
+
+    protected function registerAutoloadPrefix($prefix, $base_dir)
+    {
+        // Taken from http://www.php-fig.org/psr/psr-4/examples/
+        
+        spl_autoload_register(function ($class) use ($prefix, $base_dir) 
+        {
+            // does the class use the namespace prefix?
+            
+            $len = strlen($prefix);
+            
+            if (strncmp($prefix, $class, $len) !== 0) 
+            {
+                // no, move to the next registered autoloader
+                return;
+            }
+
+            // get the relative class name
+            $relative_class = substr($class, $len);
+
+            // replace the namespace prefix with the base directory, replace namespace
+            // separators with directory separators in the relative class name, append
+            // with .php
+            $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+            // if the file exists, require it
+            if (file_exists($file)) 
+            {
+                require $file;
+            }
+        });
+    }
+    
+    
+    public function registerAutoloadFile($autoload_file)
+    {
+        if (file_exists($autoload_file))
+        {
+            require_once $autoload_file;
+        }
+    }
+}

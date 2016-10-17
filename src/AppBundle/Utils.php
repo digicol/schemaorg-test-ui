fd8320add1271c3c2654f132ca153af001d49b7d
Tim Strehle <tim@strehle.de>
2016-10-17 16:59:38 +0200

    Changing image file references to the structure outlined in https://www.strehle.de/tim/weblog/archives/2015/12/04/1577

--- src/AppBundle/Utils.php
+++ src/AppBundle/Utils.php
@@ -32,11 +32,72 @@ class Utils
         $properties = $thing->getProperties();
         
         $properties[ 'digicol:reconciled' ] = $thing->getReconciledProperties($properties); 
+        
+        // Some content sources provide multiple thumbnails; find the largest one
+        // (to show on the details page)
+        
+        $thumbnails = [ ];
+        
+        if (! empty($properties['thumbnail']))
+        {
+            foreach ($properties['thumbnail'] as $thumbnail)
+            {
+                $thumbnails[ ] = $thumbnail;
+            }
+        }
+        elseif (isset($properties['associatedMedia']) && is_array($properties['associatedMedia']))
+        {
+            foreach ($properties['associatedMedia'] as $media)
+            {
+                if (! isset($media['thumbnail']))
+                {
+                    continue;
+                }
 
+                foreach ($media['thumbnail'] as $thumbnail)
+                {
+                    $thumbnails[ ] = $thumbnail;
+                }
+            }
+        }
+
+        $properties['digicol:reconciled']['digicol:largestThumbnailUrl'] = $this->getLargestThumbnailUrl
+        (
+            $thumbnails, 
+            $properties['digicol:reconciled']['thumbnailUrl']
+        );
+        
         return $properties;
     }
 
 
+    protected function getLargestThumbnailUrl(array $thumbnails, $default_url)
+    {
+        $result = $default_url;
+        
+        $url_to_size = [ ];
+        
+        foreach ($thumbnails as $thumbnail)
+        {
+            if (empty($thumbnail['contentUrl']) || empty($thumbnail['width']) || empty($thumbnail['height']))
+            {
+                continue;
+            }
+            
+            $url_to_size[ $thumbnail['contentUrl'] ] = intval($thumbnail['width']) * intval($thumbnail['height']);
+        }
+
+        asort($url_to_size);
+        
+        foreach ($url_to_size as $url => $size)
+        {
+            $result = $url;
+        }
+        
+        return $result;
+    }
+    
+    
     protected function registerAutoloadPrefix($prefix, $base_dir)
     {
         // Taken from http://www.php-fig.org/psr/psr-4/examples/
